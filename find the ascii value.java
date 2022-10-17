be4add10c62fe51a700a539f74473f1f016d45ae
MOHAMED YUSUF <111886678+yusufsmart@users.noreply.github.com>
2022-10-17 06:58:13 +0530

    Add files via upload

---
+++ find the ascii value.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	 	char ch = 'H'; 
+        int ascii = ch; 
+        // You can also cast char to int
+        int castAscii = (int) ch; 
+        System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii); 
+   } 
+}
+	
+
